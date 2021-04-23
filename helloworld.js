ad9ee37c517a54dd58e2c524c88a1dccff3659f1
ArunMannayan <arunpaul098@gmail.com>
2021-04-23 23:42:57 +0530

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -2,7 +2,7 @@ const port = 3000
 require('http')
   .createServer((req, res) => {
     console.log('url:', req.url)
-    res.end('Hello World')
+    res.end('Hello World 2021')
   })
   .listen(port, (error)=>{
     console.log(`server is running on ${port}`)
